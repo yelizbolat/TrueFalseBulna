0cc2cd54aaab555a643e50e20c4e75f2d806ae5b
bolat <bolat@YELIZ>
2023-11-28 11:47:25 +0300

    Proje dosyası ekle.

---
+++ TrueFalseMetodu/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueFalseMetodu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrueFalse sayibul = new TrueFalse();
+            bool sonuc = sayibul.TekCift(10);
+            if (sonuc)
+            {
+                Console.WriteLine("Sayı tektir");
+            }
+            else
+            {
+                Console.WriteLine("sayı çifttir");
+            }
+        }
+    }
+    public class TrueFalse
+    {
+        public bool TekCift(int sayi)
+        {
+            if (sayi % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
